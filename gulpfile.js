70041d2c2e626baea004f9115dcb904e901d94e2
Diogo Franco <diogo@diogofranco.com.br>
2019-04-09 23:22:13 -0300

    configured and installed gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,22 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var rename = require('gulp-rename');
+const minify = require('gulp-minify');
+ 
+gulp.task('compress', function() {
+  gulp.src(['./frontend/js/main.js'])
+    .pipe(minify())
+    .pipe(rename('app.min.js'))
+    .pipe(gulp.dest('./public/build/js/'))
+});
 
-gulp.task('hello', function() {
-    console.log('Hello Zell');
-  });
+ 
+sass.compiler = require('node-sass');
+ 
+gulp.task('sass', function () {
+  return gulp.src('./frontend/sass/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(rename('app.min.css'))
+    .pipe(gulp.dest('./public/build/css/'));
+});
+
--- public/build/js/app.js
+++
@@ -1,1 +0,0 @@
-var dev = "Diogo Franco";
