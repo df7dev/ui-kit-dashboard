ddaed26c8441f825b92701d7f4058530e3c11c44
Diogo <Franco>
2019-04-13 19:37:40 -0300

    add carts from charts

--- public/index.php
+++ public/index.php
@@ -1,111 +1,44 @@
 <?php include'layouts/top.php'; ?>
 <!-- Page Content Holder -->
-<div class="content-page">
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
-  fefwe <br>
+<div class="content-page pt-3">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-4">
+        <!-- Chart -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- Chart -->
+      </div>
+      <div class="col-md-4">
+        <!-- Chart -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- Chart -->
+      </div>
+      <div class="col-md-4">
+        <!-- Chart -->
+        <div class="card">
+          <div class="card-body">
+            <h5 class="card-title">Special title treatment</h5>
+            <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
+            <a href="#" class="btn btn-primary">Go somewhere</a>
+          </div>
+        </div>
+        <!-- Chart -->
+      </div>
+    </div>
+
+  </div>
 
 </div>
 
