cb098464c77709fe32d0821225cc4bf87704b8df
Diogo <Franco>
2019-04-13 19:36:59 -0300

    removed <ul> mob

--- public/layouts/sidebar.php
+++ public/layouts/sidebar.php
@@ -1,39 +1,43 @@
+
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <a class="navbar-brand" href="#">Navbar</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Link</a>
+      </li>
+      <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          Dropdown
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+          <a class="dropdown-item" href="#">Action</a>
+          <a class="dropdown-item" href="#">Another action</a>
+          <div class="dropdown-divider"></div>
+          <a class="dropdown-item" href="#">Something else here</a>
+        </div>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link disabled" href="#">Disabled</a>
+      </li>
+    </ul>
+    <form class="form-inline my-2 my-lg-0">
+      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
+      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+  </div>
+</nav>
 <!-- Sidebar Holder -->
 <div class="left-side-menu" data-simplebar data-simplebar-auto-hide="false">
     <div id="jquery-accordion-menu" class="jquery-accordion-menu">
         <div class="jquery-accordion-menu-header">DF Dashboard </div>
-        <!-- SIDE BAR MOBILE -->
-        <ul id="menu-tab" class="">
-            <li class="active"><a href="#"><i class="fa fa-home"></i></a></li>
-            <li><a href="#"><i class="fas fa-toolbox"></i></a></li>
-            <li><a href="#"><i class="fas fa-table"></i></a></li>
-            <li><a href="#"><i class="far fa-calendar-alt"></i></a><span class="jquery-accordion-menu-label">12 </span></li>
-            <li><a href="#"><i class="fa fa-cog"></i></a>
-                <ul class="submenu">
-                    <li><a href="#">
-                    Web Design </a></li>
-                    <li><a href="#">Hosting </a></li>
-                    <li><a href="#">Design </a>
-                        <ul class="submenu">
-                            <li><a href="#">Graphics </a></li>
-                            <li><a href="#">Vectors </a></li>
-                            <li><a href="#">Photoshop </a></li>
-                            <li><a href="#">Fonts </a></li>
-                        </ul>
-                    </li>
-                    <li><a href="#">Consulting </a></li>
-                </ul>
-            </li>
-            <li><a href="#"><i class="fab fa-wpforms"></i></a></li>
-            <li><a href="#"><i class="fas fa-toolbox"></i></a>
-                <ul class="submenu">
-                    <li><a href="#">Web Design </a></li>
-                    <li><a href="#">Graphics </a><span class="jquery-accordion-menu-label">10 </span></li>
-                    <li><a href="#">Photoshop </a></li>
-                    <li><a href="#">Programming </a></li>
-                </ul>
-            </li>
-        </ul>
         <!-- SIDE BAR DESKTOP -->
         <ul id="menu-desk" class="d-lg-none d-md-none">
             <li class="active"><a href="#"><i class="fa fa-home"></i>Dashboard </a></li>
