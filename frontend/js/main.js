6021ccc9d4af3f0eefe862971279f02bf7613df2
Diogo <Franco>
2019-04-17 01:57:45 -0300

    imported charts js

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,6 @@
 import 'jquery';
 import 'bootstrap';
-import '@fortawesome/fontawesome-free';
+import '../../node_modules/@fortawesome/fontawesome-free/js/all';
 import 'chart.js';
+import 'chartjs-plugin-datalabels';
 import './custom.js';
