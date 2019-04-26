f5b96a6abfb999fe37257740a30c9cf0bb63f90d
Diogo <Franco>
2019-04-26 20:52:53 -0300

    change direction of js

--- public/layouts/footer.php
+++ public/layouts/footer.php
@@ -3,7 +3,8 @@
       
     </div>
     <!-- JS -->
-    <script src="../build/js/main.js"></script>
+    <!-- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.0/jquery.min.js"></script> -->
+    <script src="build/js/main.js"></script>
     
   </body>
 </html>
