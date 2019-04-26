a0cd95e71af84a2adfb33037a7dfba63fcd2d8d1
Diogo <Franco>
2019-04-26 20:53:30 -0300

    custom head

--- public/layouts/top.php
+++ public/layouts/top.php
@@ -2,17 +2,47 @@
 <html lang="en">
   <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+
     <meta name="description" content="">
     <meta name="author" content="">
     <link rel="icon" href="../../../../favicon.ico">
 
     <title>DF Dashboard</title>
 
-    <link rel="canonical" href="https://getbootstrap.com/docs/4.1/examples/dashboard/">
+    <meta name="description" content="Dashboard Design por @diogofrancoweb">
+
+    <link rel="canonical" href="https://diogofranco.com.br">
+    <link rel="alternate" type="application/rss+xml" title="Marabesi" href="">
+
+    <meta property="og:title" content="" />
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="https://diogofranco.com.br" />
+    <meta property="og:image" content="https://www.diogofranco.com.br/assets/img/logo.png" />
+    <meta property="og:description" content="Dashboard Design por @diogofrancoweb" />
+
+
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:site" content="@diogofrancoweb">
+    <meta name="twitter:title" content="">
+    <meta name="twitter:description" content="Dashboard Design por @diogofrancoweb">
+    <meta name="twitter:creator" content="@DiogoFrancoWeb">
 
     <!-- Custom styles for this sidebar -->
-    <link href="../build/css/main.css" rel="stylesheet">
+    <link href="build/css/main.css" rel="stylesheet">
+
+    <!-- See https://goo.gl/qRE0vM -->
+    <meta name="theme-color" content="#3A384A">
+
+    <!-- Add to homescreen for Chrome on Android. Fallback for manifest.json -->
+    <meta name="mobile-web-app-capable" content="yes">
+    <meta name="application-name" content="Diogo Franco">
+
+    <!-- Add to homescreen for Safari on iOS -->
+    <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
+    <meta name="apple-mobile-web-app-title" content="Diogo Franco">
+
   </head>
 
   <body>
