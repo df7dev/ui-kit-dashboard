e9874f9e58a897fdcc378e941bb34fb728adfd3d
Diogo <Franco>
2019-04-26 20:51:20 -0300

    import jquery and definied $

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,6 +1,22 @@
-import 'jquery';
+import $ from 'jquery';
+window.jQuery = $;
+window.$ = $;
+console.log( "Desenvolvido por: @diogofrancoweb" );
 import 'bootstrap';
 import '../../node_modules/@fortawesome/fontawesome-free/js/all';
 import 'chart.js';
 import 'chartjs-plugin-datalabels';
 import './custom.js';
+
+$(document).ready(function() {
+    $('.table-dashboard tbody tr').click(function(event) {
+        if (event.target.type !== 'checkbox') {
+            $(':checkbox', this).trigger('click');
+        }
+    });
+    $('.table-dashboard tbody tr').on('touchstart', '.publications', function(){
+        if (event.target.type !== 'checkbox') {
+            $(':checkbox', this).trigger('click');
+        }
+    });
+});
